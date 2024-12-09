77cbb7d23efa1cfe7f60212dd0f6a21fc1a3d936
Samuel Järvå <samuel.jarva@iths.se>
2024-12-09 16:41:56 +0100

    Add ConfigrationViewModel, MainWindowViewModel

---
+++ Labb2-EntityFramework/ViewModel/ConfigurationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_EntityFramework.ViewModel;
+
+internal class ConfigurationViewModel
+{
+    private readonly MainWindowViewModel? mainWindowViewModel;
+
+    public ConfigurationViewModel(MainWindowViewModel? mainWindowViewModel)
+    {
+        this.mainWindowViewModel = mainWindowViewModel;
+    }
+}
---
+++ Labb2-EntityFramework/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labb2_EntityFramework;
+
+namespace Labb2_EntityFramework.ViewModel;
+
+internal class MainWindowViewModel
+{
+    public ConfigurationViewModel configurationViewModel { get; }
+
+    public MainWindowViewModel()
+    {
+        configurationViewModel = new ConfigurationViewModel(this);
+    }
+}
